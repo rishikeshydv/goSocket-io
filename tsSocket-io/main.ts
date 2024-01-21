1771cc0d268b421eabe925ecefa809fb7644af7a
rishikeshydv <rishikeshadh4@gmail.com>
2024-01-21 22:37:47 +0545

    feat: uuid added

--- tsSocket-io/main.ts
+++ tsSocket-io/main.ts
@@ -1,9 +1,22 @@
+import * as express from "express";
 import {Server} from "socket.io";
+import { createServer } from "http";
+import {uuid} from "uuid";
 
-const io = new Server({ /* options */ });
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer,{ /* options */ });
 
 io.on("connection", (socket) => {
   // ...
 });
+
+// to generate a custom session ID 
+const genID= () => {
+    io.engine.generateId = (req) => {
+        return uuid.v4(); // must be unique across all Socket.IO servers
+      }
+};
+
 
 io.listen(3000);
