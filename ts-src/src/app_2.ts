c6c75f1f82599c42d452dc833b826d128eeb6b7e
rishikeshydv <rishikeshadh4@gmail.com>
2024-01-22 17:51:56 +0545

    feat: two-way communcation added

--- ts-src/src/app_2.ts
+++ ts-src/src/app_2.ts
@@ -9,15 +9,32 @@ const httpServer = createServer(app);
 const io = new Server(httpServer,{ /* options */ });
 const port = 8000;
 
+
+// to generate a custom session ID 
+// export default function genID () {
+//     io.engine.generateId = () => {
+//         return uuid.v4(); // must be unique across all Socket.IO servers
+//       }
+// };
+
+
 // whenever a user connects on port 3000 via
 // a websocket, log that a user has connected
-io.on("connection", function (socket:any) {
+io.on("connection", (socket:any) => {
     console.log("A User Has Been Connected!")
-
-    // whenever we receive a 'message' we display it on console
+    // whenever we receive a 'message' we log it out
     socket.on("message", function(message: any) {
     console.log(message);
-}); 
+
+    // Broadcast the message to all connected clients
+    io.emit('chat message', message);
+  })
+
+  //whenever we receive a disconnect signal, we
+  socket.on("disconnect", function(){
+    console.log("User Disconnected");
+  })
+  })
 
 app.get("/",(req:Request,res:Response)=>{
     res.sendFile(path.resolve("public/index.html"))
@@ -25,6 +42,4 @@ app.get("/",(req:Request,res:Response)=>{
 
 httpServer.listen(port,()=>{
     console.log(`Server is running at http://localhost:${port}`)
-})
-
 })
