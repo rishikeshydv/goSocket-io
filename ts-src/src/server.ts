83b9fa0b2fe7ee84feb8409da6edc518d226ef23
rishikeshydv <rishikeshadh4@gmail.com>
2024-01-22 19:20:31 +0545

    chore: learning added

--- ts-src/src/server.ts
+++ ts-src/src/server.ts
@@ -1,30 +1,43 @@
-// src/app.ts
 import express, { Request, Response } from 'express';
 import * as path from "path";
 import {Server} from "socket.io";
 import { createServer } from "http";
 
 const app = express();
+app.use(express.static('public'));
 const httpServer = createServer(app);
 const io = new Server(httpServer,{ /* options */ });
 const port = 8000;
 
+app.get("/",(req:Request,res:Response)=>{
+    res.render('index.ejs')
+});
+
+app.post("/room",(req:Request,res:Response)=>{
+    var roomname = req.body.roomname;
+    var username = req.body.username;
+    res.redirect(`/room?username=${username}&roomname=${roomname}`)
+})
+
+//                                    WEBSOCKETS PART
+
+
 // whenever a user connects on port 3000 via
 // a websocket, log that a user has connected
-io.on("connection", function (socket:any) {
+io.on("connection", (socket:any) => {
     console.log("A User Has Been Connected!")
-
-    // whenever we receive a 'message' we display it on console
+    // whenever we receive a 'message' we log it out
     socket.on("message", function(message: any) {
     console.log(message);
-}); 
 
-app.get("/",(req:Request,res:Response)=>{
-    res.sendFile(path.resolve("views/main.html"))
-});
+  })
+
+  //whenever we receive a disconnect signal, we
+  socket.on("disconnect", function(){
+    console.log("User Disconnected");
+  })
+  })
 
 httpServer.listen(port,()=>{
     console.log(`Server is running at http://localhost:${port}`)
-})
-
 })
--- ts-src/src/temp.ts
+++ ts-src/src/temp.ts
@@ -1,3 +1,5 @@
+// in this code, we are only doing one way communication
+
 // src/app.ts
 import express, { Request, Response } from 'express';
 import * as path from "path";
@@ -5,42 +7,26 @@ import {Server} from "socket.io";
 import { createServer } from "http";
 
 const app = express();
-app.use(express.static('public'));
 const httpServer = createServer(app);
 const io = new Server(httpServer,{ /* options */ });
 const port = 8000;
 
-
-// to generate a custom session ID 
-// export default function genID () {
-//     io.engine.generateId = () => {
-//         return uuid.v4(); // must be unique across all Socket.IO servers
-//       }
-// };
-
-
 // whenever a user connects on port 3000 via
 // a websocket, log that a user has connected
-io.on("connection", (socket:any) => {
+io.on("connection", function (socket:any) {
     console.log("A User Has Been Connected!")
-    // whenever we receive a 'message' we log it out
+
+    // whenever we receive a 'message' we display it on console
     socket.on("message", function(message: any) {
     console.log(message);
-
-    // Broadcast the message to all connected clients
-    io.emit('chat message', message);
-  })
-
-  //whenever we receive a disconnect signal, we
-  socket.on("disconnect", function(){
-    console.log("User Disconnected");
-  })
-  })
+}); 
 
 app.get("/",(req:Request,res:Response)=>{
-    res.render('index')
+    res.sendFile(path.resolve("views/main.html"))
 });
 
 httpServer.listen(port,()=>{
     console.log(`Server is running at http://localhost:${port}`)
+})
+
 })
