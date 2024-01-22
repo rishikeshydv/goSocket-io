35bbb7d0036550d9607fc8e0571940ebd565ea03
rishikeshydv <rishikeshadh4@gmail.com>
2024-01-22 17:35:58 +0545

    feat: one-way communcation added

--- ts-src/src/app.ts
+++ ts-src/src/app.ts
@@ -9,14 +9,6 @@ const httpServer = createServer(app);
 const io = new Server(httpServer,{ /* options */ });
 const port = 8000;
 
-// to generate a custom session ID 
-// export default function genID () {
-//     io.engine.generateId = () => {
-//         return uuid.v4(); // must be unique across all Socket.IO servers
-//       }
-// };
-
-
 // whenever a user connects on port 3000 via
 // a websocket, log that a user has connected
 io.on("connection", (socket:any) => {
