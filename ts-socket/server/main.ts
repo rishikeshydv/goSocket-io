b0d808b744165b335528e972cc9848e15d2595fe
rishikeshydv <rishikeshadh4@gmail.com>
2024-01-22 12:47:52 +0545

    feat: nextjs app added

---
+++ ts-socket/server/main.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import * as path from "path";
+import {Server} from "socket.io";
+import { createServer } from "http";
+//import {uuid} from "uuid";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer,{ /* options */ });
+
+io.on("connection", (socket:any) => {
+  console.log("A User Has Been Connected!")
+});
+
+app.get("/",(req:any,res:any)=>{
+    res.sendFile(path.resolve("index.html"))
+});
+
+// to generate a custom session ID 
+// export default function genID () {
+//     io.engine.generateId = () => {
+//         return uuid.v4(); // must be unique across all Socket.IO servers
+//       }
+// };
+
+
+httpServer.listen(3000);
