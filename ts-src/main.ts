653b973d1f0dc3605bf02d98e058b6745f2a32ab
rishikeshydv <rishikeshadh4@gmail.com>
2024-01-22 15:59:21 +0545

    feat: basic sockets added

--- ts-src/main.ts
+++ ts-src/main.ts
@@ -8,20 +8,6 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer,{ /* options */ });
 
-io.on("connection", (socket:any) => {
-  console.log("A User Has Been Connected!")
-});
-
-app.get("/",(req:any,res:any)=>{
-    res.sendFile(path.resolve("index.html"))
-});
-
-// to generate a custom session ID 
-// export default function genID () {
-//     io.engine.generateId = () => {
-//         return uuid.v4(); // must be unique across all Socket.IO servers
-//       }
-// };
 
 
-httpServer.listen(3001);
+
--- ts-src/src/app.ts
+++ ts-src/src/app.ts
@@ -5,12 +5,32 @@ import {Server} from "socket.io";
 import { createServer } from "http";
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer,{ /* options */ });
 const port = 8000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript Express Server!');
+// to generate a custom session ID 
+// export default function genID () {
+//     io.engine.generateId = () => {
+//         return uuid.v4(); // must be unique across all Socket.IO servers
+//       }
+// };
+
+
+// whenever a user connects on port 3000 via
+// a websocket, log that a user has connected
+io.on("connection", (socket:any) => {
+    console.log("A User Has Been Connected!")
+    // whenever we receive a 'message' we log it out
+    socket.on("message", function(message: any) {
+    console.log(message);
+  })
+}); 
+
+app.get("/",(req:Request,res:Response)=>{
+    res.sendFile(path.resolve("public/index.html"))
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+httpServer.listen(port,()=>{
+    console.log(`Server is running at http://localhost:${port}`)
+})
