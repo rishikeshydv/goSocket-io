fb41e8aaadb0d8c21c2bb68bbcd64da945b8a42d
rishikeshydv <rishikeshadh4@gmail.com>
2024-02-05 18:44:30 -0500

    feat: prisma models corrected

--- chatApp/src/server.ts
+++ chatApp/src/server.ts
@@ -4,7 +4,7 @@ import { Server } from "socket.io";
 import { createServer } from "http";
 import { type } from "os";
 import { PrismaClient } from "@prisma/client";
-import * as crypto from 'crypto';
+import * as crypto from "crypto";
 
 //setting up prisma ORM
 const prisma = new PrismaClient();
@@ -26,101 +26,94 @@ interface userCredentials {
 }
 
 var allUsers: any = {};
-var fullChat:any = {}
+var fullChat: any = {};
 
 app.get("/", (req: Request, res: Response) => {
   res.render("index.ejs");
 });
 
-app.get("/agents",(req: Request, res: Response)=>{
+app.get("/agents", (req: Request, res: Response) => {
   async function userList(): Promise<any> {
     try {
       var usersRes = await prisma.transactions.findMany();
-      res.json(usersRes)
+      res.json(usersRes);
     } catch (error) {
       console.log(error);
     }
   }
-})
+});
 //                                    WEBSOCKETS PART
 
-function getUsers(userList: any) {
-  var onlineUsers: any = [];
-  userList.forEach((element: any) => {
-    onlineUsers.push(element);
-  });
-  return onlineUsers;
-}
+// function getUsers(userList: any) {
+//   var onlineUsers: any = [];
+//   userList.forEach((element: any) => {
+//     onlineUsers.push(element);
+//   });
+//   return onlineUsers;
+// }
 
 // whenever a user connects on port 3000 via
 // a websocket, log that a user has connected
 
 io.on("connection", (socket: any) => {
   console.log("A New User Connected");
-  socket.on("broker-connect", function (data:string) {
-
+  socket.on("broker-connect", function (data: string) {
     //the roomId is hashed
-    const hash = crypto.createHash('sha256');
+    const hash = crypto.createHash("sha256");
     hash.update(data);
-    const hashedRoomId = hash.digest('hex');
+    const hashedRoomId = hash.digest("hex");
 
     //joining the socket
     socket.join(hashedRoomId);
 
     prisma.chatHistory.create({
-      data:{
-        fullChat:[],
-        roomId:hashedRoomId,
-      }
-    })
-
-      // whenever we receive a 'message' we log it out
-  socket.on("sendMessage", function (currMsg: any) {
-    socket.broadcast.to(hashedRoomId).emit("message",currMsg);
-    prisma.chatHistory.update({
-      where: {
+      data: {
+        fullChat: [],
         roomId: hashedRoomId,
       },
-      data: {
-        fullChat:{
-          push:currMsg,
-        }
-      }
-    }).then(()=> {
-      console.log("Update Successful");
-    }).catch((error)=>{
-      console.error("Error updating chat history")
-    })
+    });
   });
-  });
-
 
   //whenever we receive a disconnect signal, we
-  socket.on("disconnect", function () {
-    var rooms = Object.keys(socket.rooms);
-    var socketId = rooms[0];
-    var roomname = rooms[1];
-    allUsers[roomname].forEach((user: any, index: any) => {
-      if (user[socketId]) {
-        allUsers[roomname].splice(index, 1);
-      }
-    });
+socket.on("disconnect", function () {
+  var rooms = Object.keys(socket.rooms);
+  var socketId = rooms[0];
+  var roomname = rooms[1];
+  allUsers[roomname].forEach((user: any, index: any) => {
+    if (user[socketId]) {
+      allUsers[roomname].splice(index, 1);
+    }
+  });
+});
 
-    //Broadcasting the user who is typing
+// whenever we receive a 'message' we log it out
+// socket.on("sendMessage", function (currMsg: any) {
+//   socket.broadcast.to(hashedRoomId).emit("message",currMsg);
+//   prisma.chatHistory.update({
+//     where: {
+//       roomId: hashedRoomId,
+//     },
+//     data: {
+//       fullChat:{
+//         push:currMsg,
+//       }
+//     }
+//   }).then(()=> {
+//     console.log("Update Successful");
+//   }).catch((error)=>{
+//     console.error("Error updating chat history")
+//   })
+// });
+
+});
+
+//Broadcasting the user who is typing
 //     socket.on("typing", (data: any) => {
 //       socket.broadcast.to(data.roomname).emit("typing", data.username);
 //     });
 //   });
 // });
 
-
-
-
-
-
 httpServer.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
-export {userList};
-
